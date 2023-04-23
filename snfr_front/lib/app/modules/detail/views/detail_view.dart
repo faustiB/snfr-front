a907a6d98747e31a1e9c2b2c56f8d59c2f169e11
Faozi Bouybaouene Gadrouz <faozi.bouybaouene@col.vueling.com>
2023-04-23 17:33:43 +0200

    styling on detail page

--- snfr_front/lib/app/modules/detail/views/detail_view.dart
+++ snfr_front/lib/app/modules/detail/views/detail_view.dart
@@ -10,51 +10,50 @@ class DetailView extends GetView<DetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('DetailView'),
-          centerTitle: true,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+      appBar: AppBar(
+        title: Text(controller.shoe.value.title!, overflow: TextOverflow.fade),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey.shade100,
+      body: Center(
+        child: Column(
           children: [
             Image.network(controller.shoe.value.image!),
             SizedBox(height: 40),
             Text(
-              controller.shoe.value.title!,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueAccent.shade200,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
               controller.shoe.value.price!,
               style: TextStyle(
                 fontSize: 22,
+                fontWeight: FontWeight.bold,
                 color: Colors.blueAccent.shade100,
               ),
             ),
             SizedBox(height: 40),
             ElevatedButton(
-                onPressed: () {
-                  launchUrl(Uri.parse(controller.shoe.value.url!));
-                },
-                child: Container(
-                  margin: const EdgeInsets.only(top: 20, bottom: 20, left: 40, right: 40),
-                  width: 200,
-                  child: const Center(
-                    child: Text(
-                      'Buy Now',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                launchUrl(Uri.parse(controller.shoe.value.url!));
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 20, bottom: 20),
+                width: 200,
+                child: const Center(
+                  child: Text(
+                    'Buy Now',
+                    style: TextStyle(fontSize: 20, color: Colors.blueAccent),
                   ),
                 ),
+              ),
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
